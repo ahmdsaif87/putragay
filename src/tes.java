8171fec79fcd590377eac559fc42afa15c91c15e
ahmadsaif7456@gmail.com <ahmadsaif7456@gmail.com>
2023-11-12 21:50:01 +0700

    apcb

--- src/tes.java
+++ src/tes.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class tes {
+public class tes {
+//    public static void main(String[] args) {
+//        int panjang = 10;
+//        int lebar = 6;
+//        int hasil = 0;
+//
+//        hasil = panjang*lebar;
+//        System.out.println("Jadi luas putra adalah " + hasil);
+//
+//    }
+    public static void main(String[] args) {
+        int alas = 15;
+        int tinggi = 23;
+        double luas= 0.5 * alas * tinggi;
+
+        if(luas % 2 == 0){
+            System.out.println("luas" + luas + "Genap");
+        }else{
+            System.out.println("Luas" + luas + "Ganjil");
+        }
+    }
 }
+
